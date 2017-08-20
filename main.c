faf9fd3e41d1975b40e5b5d2c53df7e597fce044
yanivsc1 <31178804+yanivsc1@users.noreply.github.com>
2017-08-20 12:34:41 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "SPFIARGame.h"
+#include "SPMainAux.h"
+
+/**
+ * Main -
+ * Defines a new game, and then enters a game loop that finishes when user quits.
+ * @return
+ * 0 - when the game is over.
+ */
+int main()
+{
+    setbuf(stdout, NULL);
+    int diff;
+    SPFiarGame* spfr = NULL;
+    int result = -2;
+    while (result == -2)
+    {
+        diff = initialization();
+        if(diff == -1)
+        {
+            printQuitMessage();
+            return 0;
+        }
+        spfr = spFiarGameCreate(HISTORY_SIZE);
+        spfr->currentPlayer = SP_FIAR_GAME_PLAYER_1_SYMBOL;
+        result = Game(spfr, diff);
+        spFiarGameDestroy(spfr);
+    }
+
+	return 0;
+}
